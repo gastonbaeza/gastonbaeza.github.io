190d63a9abdf586ac79456b27bd68e9165846a54
gastonbaeza <gaston.baeza@mightyhive.com>
2023-03-11 23:42:54 -0300

    update

---
+++ cloud-function/main.py
@@ -0,0 +1,20 @@
+import os
+import json
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+def main(request):
+    message = Mail(
+        from_email='baezagaston@gmail.com',
+        to_emails='gaston.baeza@mightyhive.com',
+        subject='Sending with Twilio SendGrid is Fun',
+        html_content='<strong>and easy to do anywhere, even with Pythonqqqqqq</strong>')
+    try:
+        #sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        sg = SendGridAPIClient('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')
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e)
