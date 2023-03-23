70860b3218684ba6ba8c4e739fc8c3e761c2c0d0
gastonbaeza <gaston.baeza@mightyhive.com>
2023-03-22 21:24:34 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -2,16 +2,11 @@ var scrollTrigger = 200;
 const mybutton = document.getElementById("topBtn");
 
 window.onscroll = function() {
-  // We add pageYOffset for compatibility with IE.
-  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
-    document.getElementsByTagName("header")[0].classList.add("header-on-scroll");
-  } else {
-    document.getElementsByTagName("header")[0].classList.remove("header-on-scroll");
-  }
-
   if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650) {
     mybutton.style.display = "block";
+    document.getElementsByTagName("header")[0].classList.add("header-on-scroll");
   } else {
     mybutton.style.display = "none";
+    document.getElementsByTagName("header")[0].classList.remove("header-on-scroll");
   }
 }
