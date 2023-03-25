df103bf468b1359a222255438e8be4d1be689b9e
gastonbaeza <gaston.baeza@mightyhive.com>
2023-03-25 00:02:57 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-var scrollTrigger = 200;
 const mybutton = document.getElementById("topBtn");
 
 window.onscroll = function() {
-  if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650) {
-    mybutton.style.display = "block";
+  if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
     document.getElementsByTagName("header")[0].classList.add("header-on-scroll");
   } else {
-    mybutton.style.display = "none";
     document.getElementsByTagName("header")[0].classList.remove("header-on-scroll");
   }
+  if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
